ad778e194d315986a62665f457e3bbd03c8d3d53
Katerina-Kor <katerina.korobeiko@gmail.com>
2023-05-05 16:31:18 +0400

    feat: rewrite write-file task using readline interface

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,23 +1,47 @@
+const readline = require('node:readline');
 const { stdin, stdout } = process;
 const fs = require('fs');
 const path = require('path');
 
-const output = fs.createWriteStream(path.join(__dirname, 'text.txt'), 'utf-8');
+const read = readline.createInterface({ input: stdin, output: stdout});
+const writeToFile = fs.createWriteStream(path.join(__dirname, 'text.txt'), 'utf-8');
 
 stdout.write('For exit type key word "exit" or press Ctrl+C\n')
 stdout.write('Enter your phrases below:\n');
-stdin.on('data', (data) => {
-  if (data.toString().trim() === 'exit') {
-    process.exit();
+read.on('line', (line) => {
+  if (line.toString().trim() === 'exit') {
+    read.close();
   } else {
-    output.write(data)
+    writeToFile.write(`${line}\n`)
   }
-});
-process.on('exit', () => {
+})
+read.on('close', () => {
   stdout.write(`Thanks! We wrote your phrases on ${path.join(__dirname, 'text.txt')} file`);
   process.kill(0);
-});
-process.on('SIGINT', () => {
-  stdout.write(`Thanks! We wrote your phrases on ${path.join(__dirname, 'text.txt')} file`);
-  process.kill(0);
-});
+})
+
+
+
+// const { stdin, stdout } = process;
+// const fs = require('fs');
+// const path = require('path');
+
+// const output = fs.createWriteStream(path.join(__dirname, 'text.txt'), 'utf-8');
+
+// stdout.write('For exit type key word "exit" or press Ctrl+C\n')
+// stdout.write('Enter your phrases below:\n');
+// stdin.on('data', (data) => {
+//   if (data.toString().trim() === 'exit') {
+//     process.exit();
+//   } else {
+//     output.write(data)
+//   }
+// });
+// process.on('exit', () => {
+//   stdout.write(`Thanks! We wrote your phrases on ${path.join(__dirname, 'text.txt')} file`);
+//   process.kill(0);
+// });
+// process.on('SIGINT', () => {
+//   stdout.write(`Thanks! We wrote your phrases on ${path.join(__dirname, 'text.txt')} file`);
+//   process.kill(0);
+// });
