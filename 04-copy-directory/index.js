2f09f8b8c63c0230c5bb1a160d5c71e7c9344ae8
Katerina-Kor <katerina.korobeiko@gmail.com>
2023-05-08 13:15:46 +0400

    feat: implement copy-directory task decision

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const promises = require('fs/promises');
+const path = require('path');
 
+function copyDir(dirName) {
+  const copyDirName = path.join(path.dirname(dirName), `${path.basename(dirName)}-copy`);
+
+  const copyDir = promises.mkdir(copyDirName, {recursive: true});
+  copyDir.catch(error => console.log(error));
+
+  const initialDirFiles = promises.readdir(dirName);
+  initialDirFiles.then(
+    result => {
+      result.forEach(file => {
+        let src = path.join(dirName, file);
+        let dest = path.join(copyDirName, file);
+        promises.copyFile(src, dest);
+      })
+    },
+    error => console.log(error)
+  )
+}
+
+copyDir(path.join(__dirname, 'files'));
